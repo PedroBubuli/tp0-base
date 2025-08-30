1bfc623f6769c89b262fbc0b15158252cafab485
Pedro <bubuli.pedro@gmail.com>
2025-08-30 17:57:35 -0300

    edito el script bash para que genere el docker-compose con los volumes de los config para server y cliente

--- generar-compose.sh
+++ generar-compose.sh
@@ -23,6 +23,8 @@ services:
     environment:
       - PYTHONUNBUFFERED=1
       - LOGGING_LEVEL=DEBUG
+    volumes:
+      - ./server/config.ini:/config.ini
     networks:
       - testing_net
 EOF
@@ -38,6 +40,8 @@ cat >> "$OUTPUT" <<EOF
     environment:
       - CLI_ID=$i
       - CLI_LOG_LEVEL=DEBUG
+    volumes:
+      - ./client/config.yaml:/config.yaml
     networks:
       - testing_net
     depends_on:
