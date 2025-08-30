63bb6751f41ad93f4ffb2498acc1d0d901292d1e
Pedro <bubuli.pedro@gmail.com>
2025-08-30 18:24:08 -0300

    fix

--- generar-compose.sh
+++ generar-compose.sh
@@ -22,7 +22,6 @@ services:
     entrypoint: python3 /main.py
     environment:
       - PYTHONUNBUFFERED=1
-      - LOGGING_LEVEL=DEBUG
     volumes:
       - ./server/config.ini:/config.ini
     networks:
@@ -39,7 +38,6 @@ cat >> "$OUTPUT" <<EOF
     entrypoint: /client
     environment:
       - CLI_ID=$i
-      - CLI_LOG_LEVEL=DEBUG
     volumes:
       - ./client/config.yaml:/config.yaml
     networks:
